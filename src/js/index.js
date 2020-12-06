cf94aacaaefefbc9912dfbb708ddd99de4e5d533
eir73 <roflaneir73@gmail.com>
2020-12-06 14:30:21 +0200

    update pages, add new + vue

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+const index = new Vue({
+  el: 'index',
+  data: {
+    /*
+    info: []
+    */
+  },
+  async mounted() {
+    console.log('index')
+    /* how to get info
+    this.info = await (await fetch('url')).json()
+    */
+  }
+})
